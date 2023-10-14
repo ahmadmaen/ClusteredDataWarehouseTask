2af1a85f669a63767cf0cf40b28b5830b4b0339e
ahmadmaen <67500666+ahmadmaen@users.noreply.github.com>
2023-10-14 10:54:36 +0300

    commit 2

--- src/main/java/ProgressSoft/example/ClusteredDataWarehouse/controller/DealRequestController.java
+++ src/main/java/ProgressSoft/example/ClusteredDataWarehouse/controller/DealRequestController.java
@@ -2,12 +2,14 @@ package ProgressSoft.example.ClusteredDataWarehouse.controller;
 
 import ProgressSoft.example.ClusteredDataWarehouse.entity.DealRequest;
 import ProgressSoft.example.ClusteredDataWarehouse.exception.CustomException.DealRequestNotFoundException;
+import ProgressSoft.example.ClusteredDataWarehouse.exception.CustomException.ValidationException;
 import ProgressSoft.example.ClusteredDataWarehouse.service.DealRequestService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -31,13 +33,25 @@ public class DealRequestController {
                     MediaType.APPLICATION_XML_VALUE
             }
     )
-    public ResponseEntity<DealRequest> createDealRequest(@Valid @RequestBody DealRequest dealRequest) {
+    public ResponseEntity<DealRequest> createDealRequest(@Valid @RequestBody DealRequest dealRequest , BindingResult bindingResult) {
+
+        //check if there is any validation error on the data.
+        if (bindingResult.hasErrors()) {
+            bindingResult.getFieldErrors().forEach(error -> {
+                throw new ValidationException(error.getDefaultMessage());
+            });
+        }
+        //once there are no errors , lets save the data on the object.
         DealRequest savedDealRequest = dealRequestService.saveDealRequest(dealRequest);
+
+        //return the object
         return new ResponseEntity<>(savedDealRequest, HttpStatus.CREATED);
+
     }
 
     @GetMapping()
     public List<DealRequest> getAllDealRequests() {
+        //return all DealRequest using dealRequestService.
         return dealRequestService.findAllDealRequests();
     }
 
@@ -48,15 +62,13 @@ public class DealRequestController {
                     MediaType.APPLICATION_XML_VALUE
             })
     public ResponseEntity<DealRequest> getDealRequestById(@PathVariable Long id) {
-
+        //get the DealRequest from the database
         Optional<DealRequest> tempDealRequest = dealRequestService.findDealRequestById(id);
 
-        System.out.println("suiii");
+        //check if the DealRequest is null
         DealRequest dealRequest = tempDealRequest.orElseThrow(() -> new DealRequestNotFoundException("the Request with this ID " + id + " not found"));
 
-        System.out.println("suiii");
-
-
+        //return the object
         return new ResponseEntity<>(dealRequest, HttpStatus.FOUND);
     }
 
---
+++ src/main/java/ProgressSoft/example/ClusteredDataWarehouse/exception/CustomException/ValidationException.java
@@ -0,0 +1,7 @@
+package ProgressSoft.example.ClusteredDataWarehouse.exception.CustomException;
+
+public class ValidationException extends RuntimeException{
+    public ValidationException(String s) {
+        super(s);
+    }
+}
